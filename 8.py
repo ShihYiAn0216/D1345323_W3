18794ed6d9583cad1a933e43ba2d7638223ea1ab
ShihYiAn0216 <D1345323@o365.fcu.edu.tw>
2024-09-26 11:56:33 +0800

    add 8.py file

---
+++ 8.py
@@ -0,0 +1,11 @@
+a=int(input("輸入五位數字:"))
+b=a//10000
+c=a//1000-b*10
+d=a//100-(b*100+c*10)
+e=a//10-d*10-b*1000-c*100
+f=a-b*10000-c*1000-d*100-e*10
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
